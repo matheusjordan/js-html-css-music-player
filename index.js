3d2111d86b76c818864f29b91f95a03640b0dc9e
matheus jordan <matheusj1599@gmail.com>
2020-03-05 10:37:37 -0300

    Finalizado projeto com uma musica

---
+++ index.js
@@ -0,0 +1,13 @@
+const artistName = document.querySelector('.artist-name');
+const musicName = document.querySelector('.music-name');
+const musicController = document.querySelector('audio');
+
+const currentMusic = {
+    title: 'Alcapone',
+    artist: 'Jovem Dex',
+    filename: '/assets/musics/jovem-dex-alcapone.mp3'
+};
+
+artistName.innerHTML = currentMusic.artist;
+musicName.innerHTML = currentMusic.title;
+musicController.src = currentMusic.filename;
---
+++ musics.js
@@ -0,0 +1,7 @@
+window.audios = [
+    {
+        title: 'Alcapone',
+        artist: 'Jovem Dex',
+        filename: '/assets/musics/jovem-dex-alcapone.mp3'
+    }
+];
